c7804c6ff190eeb1807b2a5a4c87fb0079b69632
Will <williamjamesthompson@gmail.com>
2018-08-23 16:36:36 -0500

    Minor changes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,18 +13,7 @@ module.exports = function (app) {
     });
 
 
-
-    // API POST Requests
-    // Below code handles when a user submits a form and thus submits data to the server.
-    // In each of the below cases, when a user submits form data (a JSON object)
-    // ...the JSON is pushed to the appropriate JavaScript array
-    // (ex. User fills out a reservation request... this data is then sent to the server...
-    // Then the server saves the data to the tableData array)
-    // ---------------------------------------------------------------------------
     app.post("/api/friends", function (req, res) {
-        // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-        // It will do this by sending out the value "true" have a table
-        // req.body is available since we're using the body-parser middleware
 
 
         var user = req.body;
@@ -44,31 +33,17 @@ module.exports = function (app) {
                 totalDifference += difference;
             }
 
-            // if new minimum, change the best friend index and set the new minimum for next iteration comparisons
             if (totalDifference < minimumDifference) {
                 bestFriendIndex = i;
                 minimumDifference = totalDifference;
             }
         }
 
-        // after match, add survey taker to friend array
         friendData.push(user);
-
-        //friendData.push(req.body);
-        // res.json(true);
         res.json(friendData[bestFriendIndex]);
 
 
 
     });
-
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body-parser middleware
-
-
-    // ---------------------------------------------------------------------------
-    // I added this below code so you could clear out the table while working with the functionality.
-    // Don"t worry about it!
 
 };
